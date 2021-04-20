29306707a2bffc96a8ac8c7105e7d3b46a690e9e
Alexander Batalov <alex.batalov@gmail.com>
2021-04-20 23:01:30 +0300

    Provide `JA2_DATA_DIR` default value
    Silences a couple of errors since it's inferred type was string | undefined.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 namespace ja2 {
-  export const JA2_DATA_DIR = process.env.JA2_DATA_DIR;
+  export const JA2_DATA_DIR = process.env.JA2_DATA_DIR || process.cwd();
 
   document.addEventListener('DOMContentLoaded', () => {
     WinMain(<HTMLElement>document.getElementById('ja2'));
